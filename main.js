247709cc42af3c065a298c05c81dbce59a63ffb5
xiaozhi1 <qasxed5@163.com>
2018-04-04 20:58:46 +0800

    传参方式修改及优化

--- main.js
+++ main.js
@@ -5,9 +5,26 @@ window.jQuery = function(nodeOrSelector){
     return nodes
 }
 
-window.jQuery.ajax = function(url,method,body,successFn,failFn){
+window.jQuery.ajax = function(options){
+    let url
+    if(arguments.length === 1){
+        url = options.url
+    }else if(arguments.length === 1){
+        url = arguments[0]
+        options = arguments[1]
+    }
+    let method = options.method
+    let body = options.body
+    let successFn = options.successFn
+    let failFn = options.failFn
+    let headers = options.headers
+
     let request = new XMLHttpRequest()                  
     request.open(method,url)   
+    for(key in headers){
+        let value = headers[key]
+        request.setRequestHeader(key,value)
+    }
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
             if(request.status >= 200 && request.status <300){
@@ -20,14 +37,25 @@ window.jQuery.ajax = function(url,method,body,successFn,failFn){
     request.send(body)
 }
 
-window.$ = window.jQuery
+function f1(responseText){}
+function f2(responseText){}
 
 myButton.addEventListener('click',(e)=>{
-    window.jQuery.ajax(
-        '/xxx',
-        'post',
-        'a=1&b=2',
-        (responseText)=>{ console.log(1) },
-        (request)=>{ console.log(2) }
-    )
+    let obj = {
+        url:'/xxxxxx',
+        method:'get',      //jquery的ajax这里叫type,这部分的命名和jQuery不一样，理解是一样的
+        headers:{
+            'content-type':'application/x-www-form-urlencoded',
+            'frank':'18'
+        },
+        successFn:(x)=>{
+            f1.call(undefined,x)
+            f2.call(undefined,x)
+        },
+        failFn:(x)=>{
+            console.log(x.status)
+            console.log(x.responseText)
+        }
+    }
+    window.jQuery.ajax(obj)
 })
